f34c24af5f14f5d8d6aa01eb6f2e603eb9737f82
ahmedeid2260 <a.email2260@gmail.com>
2024-08-22 13:24:22 +0300

    views href

--- modules/index.routes.js
+++ modules/index.routes.js
@@ -1,6 +1,5 @@
 const messageRouter = require("./messages/message.routes.js")
 const userRouter = require("./user/user.routes.js")
-
 module.exports.bootstrap = (app) => {
     app.get('/', (req, res) => res.josn({ message: 'Hello World!' }))
 
