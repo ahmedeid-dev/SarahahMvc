bb0c147203ba98ced1f04ea2652fa3db394bb320
ahmedeid2260 <a.email2260@gmail.com>
2024-08-23 15:04:19 +0300

    ejs config

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ const session = require('express-session')
 var MongoDBStore = require('connect-mongodb-session')(session);
 const { bootstrap } = require('./modules/index.routes.js')
 const express = require('express')
+const path = require('path')
 
 const app = express()
 const port = 3000
+
+app.set("view engine", "ejs");
+app.engine("ejs", require("ejs").__express); // Add this line to set the templating engine
+app.set("views", path.join(__dirname, "./views")); // Assuming 'views' is in same level as root folder
+
 
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
