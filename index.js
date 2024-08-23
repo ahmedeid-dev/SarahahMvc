6c2ccef72a01f4b0e03ba18014fbdadadbc8a147
ahmedeid2260 <a.email2260@gmail.com>
2024-08-23 15:05:42 +0300

    update

--- index.js
+++ index.js
@@ -9,13 +9,14 @@ const path = require('path')
 const app = express()
 const port = 3000
 
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+
 app.set("view engine", "ejs");
 app.engine("ejs", require("ejs").__express); // Add this line to set the templating engine
 app.set("views", path.join(__dirname, "./views")); // Assuming 'views' is in same level as root folder
 
 
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
 
 const store = new MongoDBStore({
     uri: 'mongodb://localhost:27017/sarahah',
