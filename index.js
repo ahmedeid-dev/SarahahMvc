6fc2d6537921c0133f0fe4a6b5c186e98c374f63
ahmedeid2260 <a.email2260@gmail.com>
2024-08-23 15:18:03 +0300

    __dirname

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const path = require('path')
 const app = express()
 const port = 3000
 
-app.use(express.static('public'))
+app.use(express.static(__dirname + '/public'))
 app.use(express.urlencoded({ extended: true }))
 
 app.set("view engine", "ejs");
