181690832f7c51a366b35ac69b37c0f5dcd0a1fe
ahmedeid2260 <a.email2260@gmail.com>
2024-08-23 12:31:45 +0300

    mongo atlas

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,7 +1,8 @@
 const { connect } = require("mongoose");
 
 module.exports.dbConnection = async () => {
-    connect('mongodb://localhost:27017/sarahah')
+    // connect('mongodb://localhost:27017/sarahah')
+    connect('mongodb+srv://aemail2260:NKK1wE5v6TYw0foM@node.rtr1h.mongodb.net/sarahah')
         .then(() => {
             console.log('Database connected');
         })
